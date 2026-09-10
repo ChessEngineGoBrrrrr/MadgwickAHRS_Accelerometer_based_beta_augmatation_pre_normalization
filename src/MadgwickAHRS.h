83ae477bb46baea440e5acd378481fabe8cf0af7
NEAT AI enjoyer <124731775+ChessEngineGoBrrrrr@users.noreply.github.com>
2026-09-10 23:54:51 +0300

    Update MadgwickAHRS.h
    Updated .h file for dynamic beta augmatation

--- src/MadgwickAHRS.h
+++ src/MadgwickAHRS.h
@@ -5,23 +5,22 @@
 // Implementation of Madgwick's IMU and AHRS algorithms.
 // See: http://www.x-io.co.uk/open-source-imu-and-ahrs-algorithms/
 //
-// From https://web.archive.org/web/20190712041101/http://www.x-io.co.uk/open-source-imu-and-ahrs-algorithms/:
-// "Open-source resources available on this website are provided under the
-// [GNU General Public Licence](https://web.archive.org/web/20190712041101/http://www.gnu.org/licenses/gpl.html)
-// unless an alternative licence is provided in source."
+// From the x-io website "Open-source resources available on this website are
+// provided under the GNU General Public Licence unless an alternative licence
+// is provided in source."
 //
 // Date			Author          Notes
 // 29/09/2011	SOH Madgwick    Initial release
 // 02/10/2011	SOH Madgwick	Optimised for reduced CPU load
 //
 //=============================================================================================
-#ifndef MadgwickAHRS_h
-#define MadgwickAHRS_h
+#ifndef Madgwick1AHRS_h
+#define Madgwick1AHRS_h
 #include <math.h>
 
 //--------------------------------------------------------------------------------------------
 // Variable declaration
-class Madgwick{
+class Madgwick1{
 private:
     static float invSqrt(float x);
     float beta;				// algorithm gain
@@ -33,13 +32,17 @@ private:
     float roll;
     float pitch;
     float yaw;
+    float maxBeta;    // max beta peak amplification
+    float hi;
+    float low;
+    float mod;
     char anglesComputed;
     void computeAngles();
 
 //-------------------------------------------------------------------------------------------
 // Function declarations
 public:
-    Madgwick(void);
+    Madgwick1(void);
     void begin(float sampleFrequency) { invSampleFreq = 1.0f / sampleFrequency; }
     void update(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
     void updateIMU(float gx, float gy, float gz, float ax, float ay, float az);
@@ -72,4 +75,3 @@ public:
     }
 };
 #endif
-
